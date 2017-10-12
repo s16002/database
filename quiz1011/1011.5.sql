d4a78169976f60c495d4230837a9012499cf4560
s16002 <s16002@std.it-college.ac.jp>
2017-10-12 14:21:23 +0900

    20171011_3

--- quiz1011/1011.5.sql
+++ quiz1011/1011.5.sql
@@ -1,4 +1,5 @@
-select last_name, hire_date, to_char(next_day(add_months(hire_date,6),'月曜日'),'fmDay," the "Ddthsp" of "Month,YYYY','nls_date_language = AMERICAN') as "REVIEW"
+select last_name, to_char(hire_date,'DD-MON-YY','nls_date_language=AMERICAN') as hire_date, to_char(next_day(add_months(hire_date,6),'月曜日'),'fmDay," the "Ddthsp" of "Month,YYYY','nls_date_language = AMERICAN') as "REVIEW"
 from employees
 order by employee_id
+fetch first 5 rows only
 /
